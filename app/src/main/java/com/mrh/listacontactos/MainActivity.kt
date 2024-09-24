6b1d74ff4e01f972dea2aa86b23698bc5d879c44
marioProfeJOYFE <127144031+marioProfeJOYFE@users.noreply.github.com>
2024-09-24 18:38:42 +0200

    clase 24/09

--- app/src/main/java/com/mrh/listacontactos/MainActivity.kt
+++ app/src/main/java/com/mrh/listacontactos/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.mrh.listacontactos
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,18 +13,37 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text2.input.TextFieldLineLimits
+import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mrh.listacontactos.ui.theme.ListaContactosTheme
@@ -47,6 +68,16 @@ class MainActivity : ComponentActivity() {
                                 Text("Lista Contactos")
                             }
                         )
+                    },
+                    floatingActionButton = {
+                        FilledIconButton(
+                            onClick = {
+
+                            },
+                            shape = RectangleShape
+                        ) {
+                            Icon(Icons.Filled.Add, contentDescription = "Localized description")
+                        }
                     }
                 ) { innerPadding ->
                     HomeView(
@@ -60,7 +91,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeView(modifier: Modifier = Modifier) {
-    val miLista = CrearListaContactos()
+    val miLista = crearListaContactos()
     Column(modifier = modifier) {
         miLista.forEach { persona ->
             ContactoCard(persona)
@@ -71,49 +102,54 @@ fun HomeView(modifier: Modifier = Modifier) {
 @Composable
 fun ContactoCard(persona: Contacto){
     Row(modifier = Modifier.fillMaxWidth().padding(12.dp)){
-        if(persona.sexo.equals("H")){
+
             Card(
                 modifier = Modifier.fillMaxWidth(),
                 colors = CardDefaults
                             .cardColors(
-                                containerColor = Color.Cyan
+                                containerColor = MaterialTheme.colorScheme.tertiaryContainer
                             )
             ){
-                Column(modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(persona.nombre)
-                    Spacer(Modifier.padding(4.dp))
-                    Text(persona.apellido)
+                Row(modifier = Modifier.fillMaxWidth().padding(12.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Card(modifier = Modifier.size(30.dp), shape = CircleShape){
+                        Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center){
+                            Text(persona.nombre.substring(0,1).uppercase(), textAlign = TextAlign.Center)
+                        }
+
+                    }
+                    Spacer(Modifier.padding(7.dp))
+                    Column(
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(persona.nombre, fontWeight = FontWeight.ExtraBold)
+                        Text(persona.apellido)
+                    }
                 }
             }
-        }else{
-            Card(modifier = Modifier.fillMaxWidth(),
-                colors = CardDefaults
-                            .cardColors(
-                                containerColor = Color.Green
-                            )
-            ){
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(persona.nombre)
-                    Spacer(Modifier.padding(4.dp))
-                    Text(persona.apellido)
-                }
-            }
-        }
+
 
     }
 }
 
-fun CrearListaContactos(): ArrayList<Contacto> {
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FormularioRegistroView() {
+    var nombre: String
+    Column {
+        /*TextField(
+            state = rememberTextFieldState(),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            label = {  },
+        )*/
+    }
+}
+
+//Funciones de datos
+fun crearListaContactos(): ArrayList<Contacto> {
     val listContactos: ArrayList<Contacto> = ArrayList()
     listContactos.add(Contacto(0,"Raul","Perez", "H",""))
     listContactos.add(Contacto(1,"Lidia","Lopez", "M",""))
-    listContactos.add(Contacto(3,"Pepe","Rodriguez", "H",""))
+    listContactos.add(Contacto(3,"repe","Rodriguez", "H",""))
     return listContactos
 }
